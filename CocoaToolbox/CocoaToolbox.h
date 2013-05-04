baed4cde71e3cfe6ade1c2bb16cd9a2e1cbeeb7c
Jason Brennan <i.jasonbrennan@gmail.com>
2013-05-04 14:51:58 -0400

    Initial Commit

---
+++ CocoaToolbox/CocoaToolbox.h
@@ -0,0 +1,13 @@
+//
+//  CocoaToolbox.h
+//  CocoaToolbox
+//
+//  Created by Jason Brennan on May-04-2013.
+//  Copyright (c) 2013 Jason Brennan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CocoaToolbox : NSObject
+
+@end
---
+++ CocoaToolbox/CocoaToolbox.m
@@ -0,0 +1,13 @@
+//
+//  CocoaToolbox.m
+//  CocoaToolbox
+//
+//  Created by Jason Brennan on May-04-2013.
+//  Copyright (c) 2013 Jason Brennan. All rights reserved.
+//
+
+#import "CocoaToolbox.h"
+
+@implementation CocoaToolbox
+
+@end
